1d0c9d61f49b5b052d08cfe4596bef6de5de03a3
Mexus <gilaldpellaeon@gmail.com>
2014-02-01 00:39:58 +0400

    Running tests from files

--- test/main.cpp
+++ test/main.cpp
@@ -2,16 +2,46 @@
 #include "config.h"
 #include "../source/dicom/dicom.h"
 
+static logxx::Log cLog("Testing");
+
+template<class T>
+bool Compare(const std::string& label, const T& etalon, const T& testing){
+        S_LOG("CompareStr");
+        if (etalon == testing)
+                return true;
+        else {
+                log(logxx::info) << "Faild comaring " << label << ": should be {" << etalon << "}, got {" << testing << "}" << logxx::endl;
+                return false;
+        }
+}
+
+bool TestFile(const std::string& filename, const std::string &surname, const std::string &pet, int id, const std::string& date){
+        S_LOG("TestFile");
+        Dicom dicomFile(filename);
+        if (dicomFile.Parse()){
+                if (Compare("surname", surname, dicomFile.GetName()) &&
+                        Compare("pet", pet, dicomFile.GetPet()) &&
+                        Compare("id", id, dicomFile.GetId()) &&
+                        Compare("date", date, dicomFile.GetDate()))
+                        return true;
+                else {
+                        log(logxx::error) << "Test failed for {" << filename << "}" << logxx::endl;
+                        return false;
+                }
+        } else {
+                log(logxx::error) << "Can't parse {" << filename << "}" << logxx::endl;
+                return false;
+        }
+}
+
 int main(){
         logxx::GlobalLogLevel(logxx::debug);
-	logxx::Log log("main");
-        Dicom dicomFile(DATA_DIR "1_16_37_40_1.dcm");
-        if (dicomFile.Parse()){
-                log(logxx::info) << "Surname: " << dicomFile.GetName() << "\n" <<
-                        "Pet: " << dicomFile.GetPet() << "\n" <<
-                        "Id: " << dicomFile.GetId() << "\n" << 
-                        "Date: " << dicomFile.GetDate() << logxx::endl;
-                return 0;
-        } else
-                return 1;
+	S_LOG("main");
+        return (
+                TestFile(DATA_DIR "1_16_37_40_1-cut.dcm", "иващенко", "тима", 8010, "20120101") &&
+                TestFile(DATA_DIR "10_21_16_57_1-cut.dcm", "егорова", "альгамбра", 31559, "19980930") &&
+                TestFile(DATA_DIR "28_10_14_45_1-cut.dcm", "Герасимчук", "Шейла", 39020, "20130528") &&
+                TestFile(DATA_DIR "31_16_16_31_1-cut.dcm", "искандерова", "матильда", 38742, "20130502")
+                ) ? 0 : 1;
+        
 }
