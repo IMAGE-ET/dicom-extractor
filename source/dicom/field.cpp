f5e3e2a064b2512f8c6e5bc62876cf48d1fad70e
Mexus <gilaldpellaeon@gmail.com>
2014-02-01 00:41:31 +0400

    Slight modifications to a parsing algorithm

--- source/dicom/field.cpp
+++ source/dicom/field.cpp
@@ -22,19 +22,21 @@ std::string Field::GetValue() const{
 
 bool Field::Load(std::ifstream& f, int shift){
 	S_LOG("Load");
-        log(logxx::debug) << "Loading file" << logxx::endl;
 	auto current = std::ios_base::cur;
 	if (f.good()){
 		f.seekg(shift, current);
 		uint8_t fieldLength(0);
-                log(logxx::debug) << "Current pos: " << std::hex << "0x" << f.tellg() << std::dec << logxx::endl;
 		fread(fieldLength);
-		f.seekg(4, current);
+                if (fieldLength == 0){
+                        log(logxx::warning) << "Field length is zero, nothing to read!" << logxx::endl;
+                        return false;
+                }
+		f.seekg(3, current);
 		std::unique_ptr<char[]> cValue(new char[fieldLength]);
 		f.read(cValue.get(), fieldLength);
 		check_error(cValue);
 		try {
-			value = cValue.get();
+			value.assign(cValue.get(), fieldLength);
 			ProcessValue();
 			return true;
 		} catch (const std::exception& e){
