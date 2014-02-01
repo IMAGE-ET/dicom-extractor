3a52ad7ff5be40d4f1216f2dd6a8b30e33625be5
Mexus <gilaldpellaeon@gmail.com>
2014-02-02 00:09:39 +0400

    Parsing directory

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,40 @@
 #include "logxx/logxx.h"
-#include "dicom/field.h"
-#include "config.h"
+#include "dicom/dicom.h"
+#include "dirent.h"
 
-int main(){
-	logxx::Log log("main");
-	log(logxx::info) << DATA_DIR << logxx::endl;
-	return 0;
+static logxx::Log cLog("extractor");
+
+int main(int argc, char **argv){
+	S_LOG("main");
+        logxx::GlobalLogLevel(logxx::warning);
+        if (argc < 2){
+                log(logxx::error) << "A path with dicom files should be specified" << logxx::endl;
+                return 1;
+        } else {
+                std::string path(argv[1]);
+                if (path[path.length()-1] != '/')
+                        path += '/';
+                DIR *dir;
+                if ((dir = opendir (path.c_str())) != nullptr) {
+                        dirent *ent;
+                        while ((ent = readdir (dir)) != NULL) {
+                                if (ent->d_type == DT_REG){
+                                        std::string fName = ent->d_name;
+                                        log(logxx::debug) << "{" << fName << "}" << logxx::endl;
+                                        Dicom dicom(path + fName);
+                                        if (dicom.Parse()){
+                                                log(logxx::notice) << "{" << fName << "} Parsed" << logxx::endl;
+                                        } else {
+                                                log(logxx::notice) << "{" << fName << "} Not parsed" << logxx::endl;
+                                        }
+                                }
+                        }
+                        closedir (dir);
+                } else {
+                        log(logxx::error) << "Can't open path {" << path << "}" << logxx::endl;
+                        return 1;
+                }
+                
+                return 0;
+        }
 }
